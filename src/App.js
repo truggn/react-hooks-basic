0439289cb50dd21096b2c5b1eea36f67bdc6f79b
= <thanhhiendevteam@gmail.com>
2021-07-02 19:33:22 +0700

    using useState and initial with change colorBox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.scss';
+import ColorBox from './component/colorbox';
 
 function App() {
   return (
     <div className="app">
       <h1>Welcome React Hooks!</h1>
+      <ColorBox />
     </div>
   );
 }
---
+++ src/component/colorbox/index.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './Colorbox.scss'
+
+ColorBox.propTypes = {
+
+};
+
+function getRandomColor() {
+    const color_List = ['deeppink', 'green', 'yellow', 'black', 'red', 'blue']
+
+    const RandomIndex = Math.trunc(Math.random() * 5);
+    return color_List[RandomIndex]
+}
+
+// khai báo state using hook
+function ColorBox() {
+
+    const initColor = localStorage.getItem('box_color') || 'deeppink'
+
+    const [color, setColor] = useState(() => {
+        const initColor = localStorage.getItem('box_color') || 'deeppink'
+        return initColor
+    });
+
+    function handleBoxClick() {
+        const newColor = getRandomColor(); // khi click new màu mới và set lại cái state mới
+        setColor(newColor);
+        localStorage.setItem('box_color', newColor)
+    }
+
+    return (
+        <div className='color-box'
+            style={{ backgroundColor: color }}
+            onClick={handleBoxClick}
+        >
+        </div>
+    );
+}
+
+export default ColorBox;
